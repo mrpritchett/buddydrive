77da860f8786929b29d1b372d9e3f0ed946add5e
imath <math.viet@gmail.com>
2015-09-15 13:27:16 +0200

    1.3.2 is there to fix some regressions introduced in 1.3.1

--- buddydrive.php
+++ buddydrive.php
@@ -3,7 +3,7 @@
 Plugin Name: BuddyDrive
 Plugin URI: http://imathi.eu/tag/buddydrive/
 Description: A plugin to share files, the BuddyPress way!
-Version: 1.3.1
+Version: 1.3.2
 Author: imath
 Author URI: http://imathi.eu/
 License: GPLv2
@@ -104,7 +104,7 @@ class BuddyDrive {
 
 		/** Version ***********************************************************/
 
-		$this->version    = '1.3.1';
+		$this->version    = '1.3.2';
 
 		/** Paths *************************************************************/
 
