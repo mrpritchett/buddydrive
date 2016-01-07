0aee757b09f61ee2224d4f302a5b8e4e9711c110
imath <math.viet@gmail.com>
2016-01-07 13:35:03 +0100

    Prepare BuddyDrive for 1.3.3 upgrade

--- buddydrive.php
+++ buddydrive.php
@@ -3,7 +3,7 @@
 Plugin Name: BuddyDrive
 Plugin URI: http://imathi.eu/tag/buddydrive/
 Description: A plugin to share files, the BuddyPress way!
-Version: 1.3.2
+Version: 1.3.3
 Author: imath
 Author URI: http://imathi.eu/
 License: GPLv2
@@ -104,7 +104,7 @@ class BuddyDrive {
 
 		/** Version ***********************************************************/
 
-		$this->version    = '1.3.2';
+		$this->version    = '1.3.3';
 
 		/** Paths *************************************************************/
 
